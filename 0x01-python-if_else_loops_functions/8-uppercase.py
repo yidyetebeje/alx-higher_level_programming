6e3444a82146547d86e02d9da54da661c7845bb9
yidyedelina <yidnekachewtebeje@gmail.com>
2022-05-31 19:06:49 +0300

    pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     for c in str:
         n = ord(c)
         if(n >= 97 and n <= 122):
-            n  = n - 32
+            n = n-32
         print("{}".format(chr(n)), end="")
     print("")
-    
-uppercase("hello world")
