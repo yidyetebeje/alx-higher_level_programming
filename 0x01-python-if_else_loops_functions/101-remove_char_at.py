6abb102fe21ec5eea7a8867135b520e4980753f0
yidyedelina <yidnekachewtebeje@gmail.com>
2022-05-31 22:34:06 +0300

    update

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,9 +3,7 @@ def remove_char_at(str, n):
     strcp = ""
     if(n < 0):
         return str
-    for i in range(0,len(str)):
+    for i in range(0, len(str)):
         if(i != n):
             strcp = strcp + str[i]
     return strcp
-
-print(remove_char_at("The brother of kma", 2))
