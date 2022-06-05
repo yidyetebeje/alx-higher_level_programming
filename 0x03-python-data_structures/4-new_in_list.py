9640af654572887cb9bed888ecd28952844083ae
yidyedelina <yidnekachewtebeje@gmail.com>
2022-06-05 19:38:37 +0300

    3rd question

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,9 @@ from hashlib import new
 
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
+    new_list = []
+    for i in range(0, len(my_list)):
+        new_list.append(my_list[i])
     if idx < 0 or idx >= len(my_list):
         return new_list
     else:
