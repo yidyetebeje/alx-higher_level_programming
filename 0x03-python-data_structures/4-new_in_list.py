287e73dd98961c9e043d7285ff4bcde041e4196a
yidyedelina <yidnekachewtebeje@gmail.com>
2022-06-05 19:36:23 +0300

    4 question

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from hashlib import new
+
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
