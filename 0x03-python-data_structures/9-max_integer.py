cecae1d0d68985a5e379a20af247cd519acb00f8
yidyedelina <yidnekachewtebeje@gmail.com>
2022-06-06 18:30:18 +0300

    update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
+    if len(my_list) == 0:
+        return None
     for li in my_list:
         if max <= li:
             max = li
