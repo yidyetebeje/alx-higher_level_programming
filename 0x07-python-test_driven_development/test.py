3ebfd2606389ae510e2800554afe1e0b542005ae
yidyedelina <yidnekachewtebeje@gmail.com>
2022-06-29 00:24:35 +0300

    updates'

---
+++ 0x07-python-test_driven_development/test.py
@@ -0,0 +1,9 @@
+def uniq(list):
+    """ Returns unique values of a list """
+    u_list = []
+    for item in list:
+        if item not in u_list:
+            u_list.append(item)
+    return u_list
+
+print(uniq([1,2,1]))
