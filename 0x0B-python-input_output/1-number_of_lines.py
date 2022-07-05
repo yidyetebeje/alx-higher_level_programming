9608a7a91b259b043e8df2b4f65c54925960e900
yidyedelina <yidnekachewtebeje@gmail.com>
2022-07-05 22:30:56 +0300

    python assignment

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,12 @@
+"""initializate"""
+
+
+def number_of_lines(filename=""):
+    """function that returns the number of lines of a text file"""
+
+    num = 0
+    with open(filename, encoding="utf-8") as f:
+        line = f.readlines()
+        num = len(line)
+    f.close()
+    return num
